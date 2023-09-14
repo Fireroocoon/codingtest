66fae84f03b6dd1cf793edbdea505b10293a767c
Fireroocoon <129505098+Fireroocoon@users.noreply.github.com>
2023-09-14 21:37:44 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] v = new int[n];
+        for(int j = 0; j < n; j++) {
+            v[j] = num_list[j];
+        }
+        return v;
+    }
+}
